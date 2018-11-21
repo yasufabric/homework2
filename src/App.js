dd9fa3110ed51b38f34a5004d51a6b4c148af9da
Yasufabric <yasufabric1192@gmail.com>
2018-11-21 16:29:29 +0900

    created header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
 import React, { Component } from "react";
 import "./App.css";
-import logo-white from "./assets/liven-logos/liven-logo-white-ee1baa2fc3f9ed337b966e4fdea3bb85cc962cf99587b30f52deb2eab7578732.svg";
+import logoWhite from "./assets/liven-logos/liven-logo-white-ee1baa2fc3f9ed337b966e4fdea3bb85cc962cf99587b30f52deb2eab7578732.svg";
+import logoRed from "./assets/liven-logos/liven-logo-red-90dd22ba307ff1e59019e21d999b4615bc63b821d16d92ecc465ceaf3924837c.svg";
 
 class App extends Component {
   render() {
     return (
       <div className="site-wrapper">
-        <header id="header">
+        <header id="header" class>
           <div className="wrapper">
-            <div className="header-content"><a href="#"><img src="/assets/liven-logos/liven-logo-white-ee1baa2fc3f9ed337b966e4fdea3bb85cc962cf99587b30f52deb2eab7578732.svg"></img></a></div>
+            <div className="header-content">
+              <a class="home" href="/">
+                <img
+                  className="logo-white"
+                  src={logoWhite}
+                  alt="Liven logo white"
+                />
+                <img className="logo-red" src={logoRed} alt="Liven logo red" />
+              </a>
+              <nav>
+                <a
+                  target="blank"
+                  className="sm-screen-hide"
+                  href="http://youtube.com/user/livenaustralia"
+                >
+                  Videos
+                </a>
+                <a
+                  target="blank"
+                  className="sm-screen-hide"
+                  href="http://liven.blog"
+                >
+                  Blog
+                </a>
+                <a className="sm-screen-hide" href="/merchant.html#partner">
+                  Partner
+                </a>
+                <a
+                  className="sm-screen-hide"
+                  href="https://app.liven.com.au/F4kq7V1A4P"
+                >
+                  Download
+                </a>
+                <a
+                  className="sm-screen-hide btn btn-white"
+                  href="https://livenpay.io"
+                >
+                  Token Sale
+                </a>
+                <i
+                  id="hamburger"
+                  className="fa fa-bars hamburger"
+                  aria-hidden="true"
+                />
+              </nav>
+            </div>
           </div>
         </header>
+        <section className="banner" />
       </div>
     );
   }
